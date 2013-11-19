f9256a36a4ba539427964850f6fab68a6500e2dd
reginaldolopes <reginaldoflopes@gmail.com>
2013-11-19 10:05:41 -0200

    inicio do projeto

---
+++ FormularioLogin/src/VitrineApp.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VitrineApp extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+
+	}
+
+	public class ItensProperty {
+
+	}
+
+}
